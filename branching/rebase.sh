acfc14ebcc047eca99863e808db2c112d6a43653
Andrey Sopov <lechuk@yandex.ru>
2022-04-16 17:34:37 +0300

    add rebase.sh in main  i think is all right

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
+#/bin/bash
 # display command line options
 
 count=1
-<<<<<<< HEAD
 for param in "$*"; do
     echo "\$* Parameter #$count = $param"
-=======
-for param in "$@"; do
-    echo "Next parameter: $param"
->>>>>>> acda9d0 (git-rebase 2)
     count=$(( $count + 1 ))
 done
+
